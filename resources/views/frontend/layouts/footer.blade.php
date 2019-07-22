ed00d41d0fea29264e934b479714f0df58d7a826
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-22 16:01:59 +0600

    Update footer.blade.php

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -5,7 +5,7 @@
 					<div class="col-lg-4 p-b-20">
 						<div class="size-h-3 flex-s-c">
 							<a href="index.html">
-								<img class="max-s-full" src="images/icons/logo-02.png" alt="LOGO">
+								<img class="max-s-full" src="{{URL::to('frontend/images/icons/logo-02.png')}}" alt="LOGO">
 							</a>
 						</div>
 
@@ -52,7 +52,7 @@
 						<ul>
 							<li class="flex-wr-sb-s p-b-20">
 								<a href="#" class="size-w-4 wrap-pic-w hov1 trans-03">
-									<img src="images/popular-post-01.jpg" alt="IMG">
+									<img src="{{URL::to('frontend/images/popular-post-01.jpg')}}" alt="IMG">
 								</a>
 
 								<div class="size-w-5">
@@ -70,7 +70,7 @@
 
 							<li class="flex-wr-sb-s p-b-20">
 								<a href="#" class="size-w-4 wrap-pic-w hov1 trans-03">
-									<img src="images/popular-post-02.jpg" alt="IMG">
+									<img src="{{URL::to('frontend/images/popular-post-02.jpg')}}" alt="IMG">
 								</a>
 
 								<div class="size-w-5">
@@ -88,7 +88,7 @@
 
 							<li class="flex-wr-sb-s p-b-20">
 								<a href="#" class="size-w-4 wrap-pic-w hov1 trans-03">
-									<img src="images/popular-post-03.jpg" alt="IMG">
+									<img src="{{URL::to('frontend/images/popular-post-03.jpg')}}" alt="IMG">
 								</a>
 
 								<div class="size-w-5">
