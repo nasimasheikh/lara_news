98ab02648aad435b4d7bc76615bfd4df3aa41ba7
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-22 18:22:37 +0600

    Update add_banner.blade.php

--- resources/views/frontend/layouts/add_banner.blade.php
+++ resources/views/frontend/layouts/add_banner.blade.php
@@ -1,7 +1,7 @@
 <div class="container m-t-10 m-b-15">
 	<div class="flex-c-c">
 		<a href="#">
-			<img class="max-w-full" src="{{URL::to(frontend'images/banner-01.jpg')}}" alt="IMG">
+			<img class="max-w-full" src="{{URL::to('frontend/images/banner-01.jpg')}}" alt="IMG">
 		</a>
 	</div>
 
