8c80cc6116a2ea28875a3399f567867f532004dd
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-22 18:22:40 +0600

    Update app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -6,50 +6,38 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.png"/>
+	<link rel="icon" type="image/png" href="{{URL::to('frontend/images/icons/favicon.png')}}"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/vendor/bootstrap/css/bootstrap.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/fontawesome-5.0.8/css/fontawesome-all.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/fonts/iconic/css/material-design-iconic-font.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/vendor/animate/animate.css')}}">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/vendor/css-hamburgers/hamburgers.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/vendor/animsition/css/animsition.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/util.min.css')}}">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/main.css')}}">
 <!--===============================================================================================-->
 </head>
 <body class="animsition">
 	
 	<!-- Header Start-->
-	   @include('layouts.header');
+	   @include('frontend.layouts.header');
 	<!-- Header End-->
 
-	<!-- Feature Start -->
-        @include('layouts.feature');
-	<!-- Feature End -->
-	<!-- Post Start-->
-		@include('layouts.post');
-	<!-- Post End-->
-	
 
-	<!-- AddBanner Start-->
-		@include('layouts.add_banner');
-	<!-- AddBanner End -->
+	   @yield('page_content');
 
-	<!-- Latest Start-->
-		@include('layouts.latest');
-	<!-- Latest End-->
-	
+
 
 	<!-- Footer Start-->
-		@include('layouts.footer');
+		@include('frontend.layouts.footer');
 	<!-- Footer End-->
 
 	<!-- Back to top -->
@@ -73,14 +61,14 @@
 	</div>
 
 <!--===============================================================================================-->	
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="{{asset('frontend/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src="{{asset('frontend/vendor/animsition/js/animsition.min.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="{{asset('frontend/vendor/bootstrap/js/popper.js')}}"></script>
+	<script src="{{asset('frontend/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="js/main.js"></script>
+	<script src="{{asset('frontend/js/main.js')}}"></script>
 
 </body>
 </html>
