a65c37b591f6ba705b35ffd27e6853f2832ecc66
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-28 18:56:43 +0600

    Update 2019_07_28_094955_create_messages_table.php

--- database/migrations/2019_07_28_094955_create_messages_table.php
+++ database/migrations/2019_07_28_094955_create_messages_table.php
@@ -13,7 +13,22 @@ class CreateMessagesTable extends Migration
      */
     public function up()
     {
-        //
+         Schema::create('messages', function (Blueprint $table) { 
+             $table->integer('id'); 
+             $table->text('welcoming_msg'); 
+             $table->text('bestPost_msg'); 
+             $table->text('banUsers_msg'); 
+             $table->text('banPosts_msg'); 
+             $table->text('banComments_msg'); 
+             $table->timestamps(); 
+         }); 
+  
+         DB::table('messages')->insert( 
+             array( 
+                 ['id' => 1, 'welcoming_msg' => 'Hello', 'bestPost_msg' => 'Best posts', 'banUsers_msg' => 'you are banned', 'banPosts_msg' => 'Banned Posts', 'banComments_msg' => 'Banned comment'] 
+             ) 
+         ); 
+
     }
 
     /**
@@ -23,6 +38,6 @@ class CreateMessagesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('messages');
     }
 }
