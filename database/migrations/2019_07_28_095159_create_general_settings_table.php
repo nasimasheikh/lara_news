2322bd05a5307ef8957c4a9544043eeec8558843
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-28 18:56:52 +0600

    Update 2019_07_28_095159_create_general_settings_table.php

--- database/migrations/2019_07_28_095159_create_general_settings_table.php
+++ database/migrations/2019_07_28_095159_create_general_settings_table.php
@@ -13,7 +13,22 @@ class CreateGeneralSettingsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('general__settings', function (Blueprint $table) { 
+             $table->integer('id'); 
+             $table->string('logo')->nullable(); 
+             $table->string('name'); 
+             $table->string('email'); 
+            $table->text('tags'); 
+             $table->text('description'); 
+             $table->timestamps(); 
+         }); 
+  
+         DB::table('general__settings')->insert( 
+             array( 
+                 ['id' => 1, 'name' => 'Amazing', 'email' => 'test@test.com', 'tags' => 'sport - movies - Games', 'description' => 'this is amazing website you can do whatever you want'] 
+             ) 
+         ); 
+
     }
 
     /**
@@ -23,6 +38,6 @@ class CreateGeneralSettingsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('general__settings');
     }
 }
