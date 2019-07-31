5a762fb87c43db1bba1e44109aeacae20ebe9cea
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-31 16:04:36 +0600

    Update register.blade.php

--- resources/views/backend/register.blade.php
+++ resources/views/backend/register.blade.php
@@ -51,7 +51,7 @@
                     </div>
                     <div class="signup-image">
                         <figure><img src="{{asset('admin/images/signup-image.jpg')}}" alt="sing up image"></figure>
-                        <a href="#" class="signup-image-link">I am already member</a>
+                        <a href="{{route('main.login')}}" class="signup-image-link">I am already member</a>
                     </div>
                 </div>
             </div>
